56dae08e8a846b18a9e380f0c69ae13de70b0db6
eupiteco <eupiteco@gmail.com>
2018-10-03 00:16:08 -0300

    css minificado

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 var gulp = require('gulp');
 
-var autoprefixer = require('autoprefixer');
 var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
-//var sourcemaps = require('gulp-sourcemaps');
+
+// Plugins do postcss
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 // Sass roda primeiro compilando tudo pra css, daí pra diante entra o
-// sourcemaps pra minifica, gerar mapas, fazer autoprefix etx.
-// postcss é um plugin do sourcemaps
-// autoprefixer é um plugin do postcss
+// postcss pra minificar, gerar mapas, fazer autoprefix etx.
  
 gulp.task('style', function () {
   return gulp.src('assets_src/sass/style.scss')
     .pipe(sass().on('error', sass.logError))
-		.pipe(postcss([
-				autoprefixer
+		.pipe(postcss([ //aqui passa os plugins do postcss
+				autoprefixer(), //autoprefixer, configuração na chave 'browserslist' do package.json
+				cssnano({ // minificador, presets diferentes podem ser passados
+					preset: 'default'
+				})
 			]))
     .pipe(gulp.dest('assets/css'))
 });
