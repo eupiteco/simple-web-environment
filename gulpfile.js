4016a5a0a6789e44b20c247cab1f8bccacd6b59f
= <lucas.piteco@nextt.com.br>
2019-01-08 11:41:36 -0200

    revertendo alterações, gulp 4 fica pra outra hora

--- gulpfile.js
+++ gulpfile.js
@@ -14,31 +14,31 @@ var cssnano = require('cssnano');
 
 // Sass roda primeiro compilando tudo pra css, daí pra diante entra o
 // postcss pra minificar, gerar mapas, fazer autoprefix etx.
- 
+
 gulp.task('style', function () {
-  return gulp.src('src/sass/style.scss')
-    .pipe(sass().on('error', sass.logError))
+	return gulp.src('src/sass/style.scss')
+		.pipe(sass().on('error', sass.logError))
 		.pipe(postcss([ //aqui passa os plugins do postcss
 				autoprefixer(), //autoprefixer, configuração na chave 'browserslist' do package.json
 				cssnano({ // minificador, presets diferentes podem ser passados
 					preset: 'default'
 				})
 			]))
-    .pipe(gulp.dest('assets/css'))
+		.pipe(gulp.dest('assets/css'))
 });
- 
+
 gulp.task('watch', function (){
-  gulp.watch('src/sass/*.scss', ['style']);
+	gulp.watch('src/sass/*.scss', gulp.parallel(['style']));
 });
 
 gulp.task('webserver', function() {
-    gulp.src('./')
-      .pipe(webserver({
-        livereload: true,
-        directoryListing: true,
-        open: true,
+		gulp.src('./')
+			.pipe(webserver({
+				livereload: true,
+				directoryListing: true,
+				open: true,
 				port: 8001
-      }));
-  });
+			}));
+	});
 
 gulp.task('default', ['style', 'webserver', 'watch']);
